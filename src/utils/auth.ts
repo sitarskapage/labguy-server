fa0da91c11ef447517430d034cffd7e5de2fdf52
jakubkanna <hello.jakubkanna@gmail.com>
2024-07-25 21:30:48 +0200

    Delete src/utils/auth.ts

--- src/utils/auth.ts
+++
@@ -1,6 +0,0 @@
-import { Request, Response, NextFunction } from "express";
-
-export const auth = (req: Request, res: Response, next: NextFunction) => {
-  console.log("AOOOOOOOOOOOOOO");
-  next();
-};
