a388d7bf6c6c2d5a884cd4df6bb4448c36b2abb2
jakubkanna <hello.jakubkanna@gmail.com>
2024-08-08 17:14:30 +0200

    cleanup

--- src/app.ts
+++ src/app.ts
@@ -1,37 +1,37 @@
-import express from 'express'
-import helmet from 'helmet'
-import cors from 'cors'
-import morgan from 'morgan'
-import router from './routes'
-import errorHandler from './middleware/errorHandler'
-import { limiter } from './middleware/config/limiter'
-import { authVerify, authVerifyPOST } from './middleware/auth'
+import express from "express";
+import helmet from "helmet";
+import cors from "cors";
+import morgan from "morgan";
+import router from "./routes";
+import errorHandler from "./middleware/errorHandler";
+import { limiter } from "./middleware/config/limiter";
+import { authVerify, authVerifyPOST } from "./middleware/auth";
 
 // init
-const app = express()
+const app = express();
 
 //middleware
-app.use(express.json())
-app.use(cors())
-app.use(helmet())
-app.use(morgan('dev'))
-app.use(limiter)
+app.use(express.json());
+app.use(cors());
+app.use(helmet());
+app.use(morgan("dev"));
+app.use(limiter);
 
-// serving static files
+// serving static files NOT IMPLEMENTED
 // app.use(express.static(path.join(__dirname, "public")));
 
 //verify all post requests
-app.use(authVerifyPOST)
+app.use(authVerifyPOST);
 
 // routes
-app.use('/api/', router)
+app.use("/api/", router);
 
 // errors
-app.use(errorHandler)
+app.use(errorHandler);
 
 //server
 app.listen(3000, () => {
-    console.debug('App listening on 3000')
-})
+  console.debug("App listening on 3000");
+});
 
-export default app
+export default app;
