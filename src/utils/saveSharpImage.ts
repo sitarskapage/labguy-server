fb857e1cabde6e7c0f290a5dc7f64f36326c4fd5
jakubkanna <hello.jakubkanna@gmail.com>
2024-12-12 13:08:00 +0100

    resizing images fix

--- src/utils/saveSharpImage.ts
+++ src/utils/saveSharpImage.ts
@@ -20,13 +20,22 @@ export async function saveSharpImage(
   const image = sharp(file.buffer);
   const metadata = await image.metadata();
 
-  const resizeOptions =
-    metadata.width && metadata.height && metadata.width > metadata.height
-      ? { width: 2160 }
-      : { height: 2160 };
+  // Check if resizing is needed
+  if (
+    metadata.width &&
+    metadata.height &&
+    (metadata.width > 2160 || metadata.height > 2160)
+  ) {
+    const resizeOptions =
+      metadata.width > metadata.height ? { width: 2160 } : { height: 2160 };
 
-  await image.resize(resizeOptions).jpeg({ quality: 80 }).toFile(filePath);
+    await image.resize(resizeOptions).jpeg({ quality: 80 }).toFile(filePath);
+  } else {
+    // Save the file as is if no resizing is required
+    await image.jpeg({ quality: 80 }).toFile(filePath);
+  }
 
+  //
   const isBright = await isImageBright(file.buffer);
 
   return {
