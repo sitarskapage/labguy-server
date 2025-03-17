59e227863c3d3f998fdacbed5e33a9d11c82956e
jakubkanna <hello.jakubkanna@gmail.com>
2025-03-17 22:37:29 +0100

    generate migrations

---
+++ prisma/migrations/20250317213546_/migration.sql
@@ -0,0 +1,8 @@
+-- AlterTable
+ALTER TABLE `ImageRef` MODIFY `description` LONGTEXT NULL;
+
+-- AlterTable
+ALTER TABLE `ThreedRef` MODIFY `description` LONGTEXT NULL;
+
+-- AlterTable
+ALTER TABLE `VideoRef` MODIFY `description` LONGTEXT NULL;
